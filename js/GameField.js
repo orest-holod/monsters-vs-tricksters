3dc4db3e04e77491010d7e544e4d669e439df4b8
orest-o-holod <orest.o.holod@gmail.com>
2016-03-13 20:42:39 +0200

    Reset

---
+++ js/GameField.js
@@ -0,0 +1,72 @@
+﻿/* Start Game Field */
+
+function GameField(parentDOMElement, appendToParentDOMElement) {
+
+    GameEntity.call(this, 'div', parentDOMElement, appendToParentDOMElement, ['game-field']);
+
+    this._skyLayer = new GameFieldLayer(this._DOMElement, true, 'sky');
+    this._starsLayer = new GameFieldLayer(this._DOMElement, true, 'stars');
+    this._cloudsLayer = new GameFieldLayer(this._DOMElement, true, 'clouds');
+    this._towerLayer = new GameFieldLayer(this._DOMElement, true, 'tower');
+    this._stepsLayer = new GameFieldStepsLayer(this._DOMElement, true);
+    this._menuLayer = new GameFieldMenuLayer(this._DOMElement, true);
+    this._fpsLayer = new GameFieldLayer(this._DOMElement, true, "fps");
+}
+
+GameField.prototype = Object.create(GameEntity.prototype);
+
+/* Start Public Methods */
+
+GameField.prototype.getSkyLayer = function () {
+
+    return this._skyLayer;
+}
+
+GameField.prototype.getStarsLayer = function () {
+
+    return this._starsLayer;
+}
+
+GameField.prototype.getCloudsLayer = function () {
+
+    return this._cloudsLayer;
+}
+
+GameField.prototype.getTowerLayer = function () {
+
+    return this._towerLayer;
+}
+
+GameField.prototype.getStepsLayer = function () {
+
+    return this._stepsLayer;
+}
+
+GameField.prototype.getMenuLayer = function () {
+
+    return this._menuLayer;
+}
+
+GameField.prototype.getFPSLayer = function () {
+
+    return this._fpsLayer;
+}
+
+GameField.prototype.repaint = function () {
+
+    GameEntity.prototype.repaint.call(this);
+
+    this._skyLayer.repaint();
+    this._starsLayer.repaint();
+    this._cloudsLayer.repaint();
+    this._stepsLayer.repaint();
+    this._menuLayer.repaint();
+}
+
+/* End Public Methods */
+
+/* End Game Field */
+
+
+
+
---
+++ js/index.js
@@ -0,0 +1,17 @@
+﻿/* Start Main Function */
+
+var gameWrapper = document.getElementById('game-wrapper');
+
+var game = new Game(gameWrapper);
+
+/* Start Event Listeners */
+
+document.addEventListener('keydown', game.keyDownEventHandler.bind(game), false);
+document.addEventListener('keyup', game.keyDownEventHandler.bind(game), false);
+document.addEventListener('click', game.mouseClickEventHandler.bind(game), false);
+
+/* End Event Listeners */
+
+game.start();
+
+/* End Main Function */
