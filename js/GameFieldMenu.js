47481f8aa945502e541373bea58e738c94353626
nata7890 <nkf.3310@gmail.com>
2016-03-19 20:25:05 +0200

    Responsive menu
    Little css changes to make menu responsive

--- js/GameFieldMenu.js
+++ js/GameFieldMenu.js
@@ -14,24 +14,6 @@ function GameFieldMenu(parentDOMElement, appendToParentDOMElement) {
     var facebookMenuItem = new GameFieldMenuItem(this.getDOMElement(), 'facebook-menu-item', 'FB');
     var googleMenuItem = new GameFieldMenuItem(this.getDOMElement(), 'google-menu-item', 'G+');
 
-    googleMenuItem.setX(0);
-    googleMenuItem.setY(this._menuHeader.getY() + this._menuHeader.getHeight() - 90);
-
-    facebookMenuItem.setX(0);
-    facebookMenuItem.setY(googleMenuItem.getY() + googleMenuItem.getHeight() + 15);
-
-    saveGameMenuItem.setX(0);
-    saveGameMenuItem.setY(facebookMenuItem.getY() + facebookMenuItem.getHeight() + 15);
-
-    resumeGameMenuItem.setX(0);
-    resumeGameMenuItem.setY(saveGameMenuItem.getY() + saveGameMenuItem.getHeight() + 15);
-
-    stopGameMenuItem.setX(0);
-    stopGameMenuItem.setY(resumeGameMenuItem.getY() + resumeGameMenuItem.getHeight() + 15);
-
-    playGameMenuItem.setX(0);
-    playGameMenuItem.setY(stopGameMenuItem.getY() + stopGameMenuItem.getHeight() + 15);
-    
     this._menuItems.push(playGameMenuItem);
     this._menuItems.push(stopGameMenuItem);
     this._menuItems.push(resumeGameMenuItem);
