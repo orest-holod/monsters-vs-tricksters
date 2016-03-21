3aa7ffb4e08e9fc4593fba6846bf6f54bdfc2c0f
orest-o-holod <orest.o.holod@gmail.com>
2016-03-21 02:19:52 +0200

    Remove moving background at low-fps devices

--- js/Game.js
+++ js/Game.js
@@ -323,9 +323,11 @@ Game.prototype.runGameLoop = function () {
             this._isGameOver = true;
         }
 
+        if (this._animationFrameManager._currentFPS >= this._animationFrameManager._wantedFPS) {
 
-        this._gameField.getGameFieldBackground().getGameFieldBackgroundCloudsLayer().moveBackgroundPositition();
-        this._gameField.getGameFieldBackground().getGameFieldBackgroundStarsLayer().moveBackgroundPositition();
+            this._gameField.getGameFieldBackground().getGameFieldBackgroundCloudsLayer().moveBackgroundPositition();
+            this._gameField.getGameFieldBackground().getGameFieldBackgroundStarsLayer().moveBackgroundPositition();
+        }
 
         this._gameField.getGameFieldTower().getVisibleTricksters().forEach(function (trickster) { trickster.rotate(); });
         this._gameField.getGameFieldTower().getVisibleTricksters().forEach(function (trickster) { trickster.levitate(); });
