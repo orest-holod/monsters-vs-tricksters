ed1b5a89884b89ad754e16597940ff8fb04f4628
nata7890 <nkf.3310@gmail.com>
2016-03-30 18:47:35 +0300

    Changed wav files to mp3

--- js/SoundManager.js
+++ js/SoundManager.js
@@ -1,6 +1,6 @@
 function SoundManager(parentDOMElement){
 
-  var sounds = ['sounds/Brave_World.wav', 'sounds/game_over.wav', 'sounds/2_steps.wav', 'sounds/1_jump.wav', 'sounds/got _life.wav', 'sounds/pop.wav', 'sounds/laughter.wav'];
+  var sounds = ['sounds/Brave_World.mp3', 'sounds/game_over.mp3', 'sounds/2_steps.mp3', 'sounds/1_jump.mp3', 'sounds/got _life.mp3', 'sounds/pop.mp3', 'sounds/laughter.mp3'];
 
   this.sound = document.createElement('audio');
 
